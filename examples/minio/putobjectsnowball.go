afa4c4cb3958b08ba42ba36a135465bce65d6928
kos <vbz007@hotmail.com>
2023-05-26 09:23:20 +0800

    examples: github.com/minio/minio-go/v7 => github.com/imkos/minio-go/v7

--- examples/minio/putobjectsnowball.go
+++ examples/minio/putobjectsnowball.go
@@ -29,8 +29,8 @@ import (
 	"os"
 	"time"
 
-	"github.com/minio/minio-go/v7"
-	"github.com/minio/minio-go/v7/pkg/credentials"
+	"github.com/imkos/minio-go/v7"
+	"github.com/imkos/minio-go/v7/pkg/credentials"
 )
 
 func main() {
